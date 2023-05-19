87ecb9aca860fda641b8d2eb9df53b74e2f43797
Atul Kumar Singh <atul.kumar.s.singh@pwc.com>
2023-05-19 12:24:37 +0530

    useEffect examples

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import Header from "./components/Header/Header";
-import Para from './components/Header/Para/Para';
+// import Header from "./components/Header/Header";
+// import Para from './components/Header/Para/Para';
 import './App.css'
 
 function App() {
@@ -19,7 +19,40 @@ function App() {
   //   }
   // ]);
   const [count,setCount] = useState(0)
+  //const color = count % 5 === 0 ? 'red' : 'blue';
 
+  //==================================================================
+  //this will be trigger each time component re-render
+  // useEffect(() => {
+  //   console.log(" useEFFECT----color");
+  //   document.body.style.backgroundColor = randomColor(); 
+  //   //api
+  // })
+
+  //======================================================================
+  //this will be trigger for the very first time when component render after that will be not 
+  //render
+
+ // useEffect(() => {
+  //   console.log(" useEFFECT----color");
+  //   document.body.style.backgroundColor = randomColor(); 
+  //   //api
+  // },[])
+
+  //======================================================================
+
+  //on the 3rd example we can pass array filled with all the dependancies 
+  //that should trigger the callback funtion
+  //anytime the dependencies chanegs it will trigger the callback funtion
+
+  // useEffect(() => {
+  //   console.log(" useEFFECT----color");
+  //   document.body.style.backgroundColor = randomColor(); 
+  //   //api
+  // },[color])
+
+  console.log("render=========")
+  
   return (
     <div className="App">
       {/* <Header text={`Hello PwC Developers`}/>
@@ -44,5 +77,5 @@ export default App;
 
 
 function randomColor(){
-  return `#${Math.random().toString(16).substr(-6)}`;
+  return `#${Math.random().toString(16).substr(-6)}`; 
 }
